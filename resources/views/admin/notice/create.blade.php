23ab18a7bfff691632c87f709af610aacc059e80
rifat al mamun <rifatalmamun.cse@gmail.com>
2021-05-01 01:02:37 +0600

    notice board

--- resources/views/admin/notice/create.blade.php
+++ resources/views/admin/notice/create.blade.php
@@ -8,7 +8,7 @@
     <div class="app-title">
       <div>
         <h1><i class="fa fa-edit"></i>Add Notice</h1>
-        <p>Add by admin</p>
+        <p>Added by admin</p>
       </div>
       <ul class="app-breadcrumb breadcrumb">
         <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
